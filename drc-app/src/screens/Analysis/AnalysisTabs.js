60765cd85614158e852b988e650a46093afa58b7
lailihe <laililihe@sunmoon.ac.kr>
2024-10-14 16:47:17 +0900

    feat(tabs): 탭에 아이콘 추가

--- drc-app/src/screens/Analysis/AnalysisTabs.js
+++ drc-app/src/screens/Analysis/AnalysisTabs.js
@@ -3,15 +3,35 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SuddenAcceleration from "./SuddenAcceleration";
 import SuddenBraking from "./SuddenBraking";
 import SamePedal from "./SamePedal";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon2 from 'react-native-vector-icons/Ionicons';
   
 const Tab = createBottomTabNavigator();
   
 export default function AnalysisTabs() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="급가속 분석" component={SuddenAcceleration} />
-            <Tab.Screen name="급정거 분석" component={SuddenBraking} />
-            <Tab.Screen name="페달동시사용 분석" component={SamePedal} />
+            <Tab.Screen
+                name="급가속 분석"
+                component={SuddenAcceleration}
+                options={{
+                    tabBarIcon: () => <Icon name="forwardburger" size={20} color="#000" />  ,
+                }}
+            />
+            <Tab.Screen
+                name="급정거 분석"
+                component={SuddenBraking}
+                options={{
+                    tabBarIcon: () => <Icon2 name="warning" size={20} color="#000" /> ,
+                }} 
+            />
+            <Tab.Screen
+                name="페달동시사용 분석"
+                component={SamePedal}
+                options={{
+                    tabBarIcon: () => <Icon2 name="footsteps" size={20} color="#000" />,
+                }}
+            />
         </Tab.Navigator>
     );
 }
