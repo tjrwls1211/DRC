b3414d47c472f6516d234e42f03df6ec33fa147e
lailihe <laililihe@sunmoon.ac.kr>
2024-11-01 18:13:26 +0900

    chore: 서버 API 맞게 비밀번호 체크 API 통신 함수 수정

--- drc-app/src/api/accountAPI.js
+++ drc-app/src/api/accountAPI.js
@@ -1,4 +1,4 @@
-// 계정 관련 API (로그아웃, 회원탈퇴, 닉네임 수정 등)
+// 계정 관련 API (로그아웃, 회원탈퇴, 비밀번호 확인)
 import axios from 'axios';
 import { API_URL } from "@env";
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -9,48 +9,21 @@ const apiClient = axios.create({
       "Content-Type": "application/json",
     }
   });
-
-// 현재 닉네임 가져오기 API
-export const fetchCurrentNickname = async () => {
-    try {
-        const token = await AsyncStorage.getItem('token'); // AsyncStorage에서 JWT 토큰 가져오기
-        const response = await axios.get(`${API_URL}/user/nickname`, {
-            headers: {
-                Authorization: `Bearer ${token}`, // Authorization 헤더에 토큰 추가
-                "Content-Type": "application/json",
-            }
-        });
-        return response.data.nickname; // 서버의 응답에서 닉네임 반환
-    } catch (error) {
-        console.error('현재 닉네임 가져오기 오류:', error);
-        throw error;
-    }
-};
-
-// 닉네임 수정 API
-export const changeNickname = async (newNickname) => {
-    try {
-        const token = await AsyncStorage.getItem('token'); // AsyncStorage에서 JWT 토큰 가져오기
-        const response = await axios.post(`${API_URL}/user/nickname`, 
-            { nickname: newNickname }, 
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`, // Authorization 헤더에 토큰 추가
-                    "Content-Type": "application/json",
-                }
-            }
-        );
-        return response.data; // 서버의 응답 반환
-    } catch (error) {
-        console.error('닉네임 변경 오류:', error);
-        throw error;
-    }
-};
   
 // 비밀번호 확인 API
 export const checkPassword = async (password) => {
   try {
-    const response = await axios.post("/user/checkPassword", { password });
+    const token = await AsyncStorage.getItem('token');
+    const response = await axios.post(`${API_URL}/user/verifyPassword`, 
+      { pw: password },
+      {
+          headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+          }
+      }
+  );
+    console.log("비밀번호 확인 서버 반환 데이터: ", response.data);
     return response.data.isValid;
   } catch (error) {
     console.error('비밀번호 확인 오류:', error);
