228fa38edda6ba727a13718011c079ce3af28741
KimYoungJin0525 <kimdudwls15951@gmail,com>
2024-11-03 21:29:40 +0900

    test17

--- test.py
+++ test.py
@@ -101,9 +101,15 @@ class FlippedTextLabel(QLabel):
         painter = QPainter(self)
         painter.setFont(self.font())
         painter.setRenderHint(QPainter.Antialiasing)
-        painter.translate(self.width(), self.height())
-        painter.rotate(180)  # 상하 반전
-        painter.drawText(-self.width(), -self.height() + 30, self.text[::-1])  # 텍스트 위치 조정
+    
+        # 중앙 정렬을 위해 텍스트 크기 측정
+        text_width = painter.fontMetrics().width(self.text)
+        text_height = painter.fontMetrics().height()
+    
+        #  텍스트를 중앙에 배치하고 상하좌우 반전
+        painter.translate(self.width() / 2, self.height() / 2)
+        painter.rotate(180)
+        painter.drawText(-text_width / 2, text_height / 4, self.text[::-1])
         painter.end()
 
 # GUI 클래스에서 레이아웃
@@ -120,17 +126,18 @@ class CarDisplay(QWidget):
         # 상단 레이아웃
         top_layout = QHBoxLayout()
 
-        # 브레이크 이미지 (왼쪽 상단)
         self.brake_pixmap_normal = QPixmap("brake_normal.png").transformed(QTransform().scale(-1, -1))
+        self.brake_pixmap_dark = QPixmap("brake_dark.png").transformed(QTransform().scale(-1, -1))
         self.brake_label = QLabel()
         self.brake_label.setPixmap(self.brake_pixmap_normal)
         top_layout.addWidget(self.brake_label, alignment=Qt.AlignLeft | Qt.AlignTop)
 
-        # 엑셀 이미지 (오른쪽 상단)
         self.accel_pixmap_normal = QPixmap("accel_normal.png").transformed(QTransform().scale(-1, -1))
+        self.accel_pixmap_dark = QPixmap("accel_dark.png").transformed(QTransform().scale(-1, -1))
         self.accel_label = QLabel()
         self.accel_label.setPixmap(self.accel_pixmap_normal)
         top_layout.addWidget(self.accel_label, alignment=Qt.AlignRight | Qt.AlignTop)
+
 
         main_layout.addLayout(top_layout)
 
