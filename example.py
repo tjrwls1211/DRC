c9e98aef753760b4d76c2b954a14794c73879d9c
KimYoungJin0525 <kimdudwls15951@gmail.com>
2024-11-04 14:47:22 +0900

    chore : 음성 global 수정

--- example.py
+++ example.py
@@ -195,7 +195,7 @@ prev_mqtt_state = None
 
 # 로드셀 데이터와 상태를 업데이트하는 함수    # 급발진 조건을 수정하자 accel_value < 10000 and brake_value > 5000 and speed >= 50 and rpm > 5000:
 def check_info(accel_value, brake_value):
-    global last_accel_time, is_accelerating, stop_sounds, is_playing_sounds, prev_mqtt_state
+    global last_accel_time, is_accelerating, stop_sounds, is_playing_sounds, prev_mqtt_state, last_brake_time, last_both_time, last_speed_time
     mqtt_state = None
     if 200 < accel_value < 1000 and brake_value <= 30:    
         state = "Unintended Acceleration"
