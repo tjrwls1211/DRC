b3d1de6d121e58d7b4ba5d2afd18c5cd5cd820cb
KimYoungJin0525 <kimdudwls15951@gmail.com>
2024-11-11 11:33:48 +0900

    chore : 속도 텍스트 위치 수정 및 긄씨크기 다운

--- test2.py
+++ test2.py
@@ -65,7 +65,7 @@ root.geometry("1000x600")
 root.configure(bg="black")
 
 # 폰트 설정
-font_large = ("Arial", 40, "bold")
+font_large = ("Arial", 35, "bold")
 
 # 속도 구간 설정
 num_bins = 10
@@ -107,7 +107,7 @@ brake_label.place(relx=-0.04, rely=0.5, anchor="w")  # 왼쪽 중앙에 배치
 
 #data부분을 나중에 속도 데이터로 넣으면될꺼같음 
 text_label = tk.Label(root, text=f"현재 ", font=font_large, bg="black", fg="white", padx=2, pady=10, width=9)
-text_label.place(relx=0.84, rely=0.05, anchor='ne')
+text_label.place(relx=0.85, rely=0.05, anchor='ne')
 
 
 # pygame 초기화
