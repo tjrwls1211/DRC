60f84a014ca6b334c3b770316bc94d4a03c3d812
lailihe <laililihe@sunmoon.ac.kr>
2024-11-30 16:12:53 +0900

    feat: 2차인증정보 모달 생성

---
+++ drc-app/src/components/Modal/TwoFactorAuthModal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, Button, TouchableOpacity, Linking } from 'react-native';
+import Modal from 'react-native-modal';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const TwoFactorAuthModal = ({ isVisible, onClose, otpKey, handleCopyOtpKey, qrUrl, isDarkMode }) => {
+  return (
+    <Modal isVisible={isVisible}>
+      <View style={{ 
+        backgroundColor: isDarkMode ? '#1f1f1f' : '#ffffff', 
+        padding: 20, 
+        borderRadius: 10,
+        alignItems: 'center',  
+        justifyContent: 'center'
+      }}>
+        <TouchableOpacity style={{ position: 'absolute', top: 10, right: 10 }} onPress={onClose}>
+          <MaterialIcons name="close" size={24} color={isDarkMode ? '#ffffff' : '#009688'} />
+        </TouchableOpacity>
+        
+        <Text style={{ 
+          color: isDarkMode ? '#ffffff' : '#009688', 
+          fontSize: 22, 
+          fontWeight: 'bold',
+          textAlign: 'center',
+          marginBottom: 15 // 제목과 다음 요소 사이 간격 추가
+        }}>
+          2차 인증 정보
+        </Text>
+
+        <View style={{ marginVertical: 10, alignItems: 'center' }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+            <Text style={{ 
+              color: isDarkMode ? '#ffffff' : '#555', 
+              textAlign: 'center',
+              marginRight: 10 // 텍스트와 복사 아이콘 사이 간격 추가
+            }}>
+              OTP 설정 키: {otpKey}
+            </Text>
+            <TouchableOpacity onPress={handleCopyOtpKey}>
+              <MaterialIcons name="content-copy" size={24} color={isDarkMode ? '#ffffff' : '#009688'} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <Button 
+          title="QR 확인" 
+          color={isDarkMode ? '#ffffff' : '#009688'} 
+          onPress={() => Linking.openURL(qrUrl)} 
+          style={{ marginTop: 15 }} // 버튼 위쪽 간격 추가
+        />
+      </View>
+    </Modal>
+  );
+};
+
+export default TwoFactorAuthModal;
